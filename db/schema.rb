5382218ea291a3e84954a9a0eeeaeb62add7f646
splmloic <l2ct@protonmail.com>
2024-03-11 17:20:07 +0100

    try to make login register look nice

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_11_104401) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "items_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["items_id"], name: "index_carts_on_items_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
