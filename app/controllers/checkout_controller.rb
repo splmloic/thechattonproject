bb2891af74755607514caffb36ab6596cf9af2f7
tommy-pellerin <tommytp@hotmail.fr>
2024-03-13 22:27:57 +0100

    creating order and emptying cart in progress

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -2,10 +2,17 @@ class CheckoutController < ApplicationController
        
     
     def create
+
+      puts "#"*50
+      puts params
+      puts "#"*50
           @total = params[:total].to_d
-          @cart_items = params[:cart_items]
+          @cart_id = params[:cart_id]
           @session = Stripe::Checkout::Session.create(
             payment_method_types: ['card'],
+            metadata: {
+              cart_id: @cart_id
+            },
             line_items: [
               {
                 price_data: {
@@ -17,9 +24,7 @@ class CheckoutController < ApplicationController
                 },
                 quantity: 1
               },
-              metadata: {
-                cart_items: @cart_items
-              },
+              
             ],
             mode: 'payment',
             success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
@@ -31,11 +36,31 @@ class CheckoutController < ApplicationController
         def success
           @session = Stripe::Checkout::Session.retrieve(params[:session_id])
           @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
-          metadata_hash = @session.metadata.to_hash
-          @cart_items = metadata_hash['cart_items']
+          @cart_id= @session.metadata.cart_id
+          @cart = Cart.find(@cart_id)          
+          # create_order(@cart)
+          # empty_cart
         end
       
         def cancel
+        end
+
+        def create_order(cart)
+          @cart = cart
+          @cart_items = @cart.cart_items
+
+          @user = current_user
+          @order = Order.new(user: @user)          
+          @order_item = 0
+          @cart_items.each do |cart_item|
+            @order_item = OrderItem.create(order: @order, item: cart_item.item, quantity: cart_item.quantity, price: cart_item.item.price)
+          end
+          @order_items = @order.order_items
+          @order.save
+        end
+
+        def empty_cart
+          current_user.cart.cart_items.destroy_all
         end
 end
 
