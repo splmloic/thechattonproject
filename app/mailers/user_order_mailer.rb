7da4c72d7f19e559553ff397c30d4f8d6bb03682
BrvAlexis <brevieralexis@gmail.com>
2024-03-14 16:56:44 +0100

    view

--- app/mailers/user_order_mailer.rb
+++ app/mailers/user_order_mailer.rb
@@ -1,13 +1,18 @@
 class UserOrderMailer < ApplicationMailer
-
+  default from: ENV['MAILJET_DEFAULT_FROM']
+  
+  
+  def order_confirmation(user, order)
+        @user = user
+        @order = order
+        @url = 'https://thechattonproject.fly.dev' 
+    
+        mail(to: @user.email, subject: 'Confirmation de commande')
+  end
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_order_mailer.order_confirmation.subject
   #
-  def order_confirmation
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
-  end
+ 
 end
